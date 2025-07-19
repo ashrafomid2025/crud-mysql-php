51e630bb1b44680c736815f3380da186bc4dde83
ashrafomid2025 <omidmashraf@gmail.com>
2025-07-19 22:48:59 +0430

    read section

--- public/read.php
+++ public/read.php
@@ -7,22 +7,23 @@ include "connection.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../src/output.css">
 </head>
 <body>
-    <table class = "border">
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Price</th>
+    <table class = "w-[80%] mx-auto border p-2 text-center">
+        <tr class="h-6">
+            <th class= "border">ID</th>
+            <th class= "border">Name</th>
+            <th class= "border">Price</th>
         </tr>
         <?php
          $dastor = "SELECT * FROM product";
         $result =  $connection->query($dastor);
          while($row = $result->fetch_assoc()){
-            echo "<tr>";
-            echo "<td>". $row["id"]. "</td>";
-            echo "<td>". $row["name"]. "</td>";
-            echo "<td>". $row["PRICE"]. "</td>";
+            echo "<tr class ='even:bg-gray-500 even:text-white'>";
+            echo "<td class ='border p-2 border-collapse '>". $row["id"]. "</td>";
+            echo "<td class ='border p-2 border-collapse '>". $row["name"]. "</td>";
+            echo "<td class ='border p-2 border-collapse '>". $row["PRICE"]. "</td>";
             echo "<tr>";
          }
         ?>
